a17bb8842800f3950c97a7e849b956af7ee7a3f9
Ayano Fujita <cathy.ayano@gmail.com>
2025-02-13 17:52:24 +0900

    Create new task

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
-def task_list(request):
+def home(request):
     tasks = Task.objects.all()
-    return render(request, 'todo/task_list.html', {'tasks': tasks})
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TaskForm()
+    return render(request, 'home.html', {'tasks': tasks, 'form': form})
